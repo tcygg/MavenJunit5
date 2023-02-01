eaf3c2b5703f4904ce5acc1a8f72077448433af3
zhangxin <1179252593@qq.com>
2023-02-01 19:11:41 +0800

    ces12

---
+++ src/main/java/com/msb/oop/test1/ceshi.java
@@ -0,0 +1,13 @@
+package com.msb.oop.test1;
+
+/**
+ * @Auther: zhaoss
+ * @Date: 2023/2/1 - 02 - 01 - 19:11
+ * @Description: com.msb.oop.test1
+ * @version: 1.0
+ */
+public class ceshi {
+    public static void main(String[] args) {
+        System.out.println("dsgdg");
+    }
+}
