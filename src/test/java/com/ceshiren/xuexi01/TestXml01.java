cfd85c2ec52cb6e56dd518c74995b15e97e84625
Tcygg <1179252593@qq.com>
2023-03-15 01:09:15 +0800

    jisj

---
+++ src/test/java/com/ceshiren/xuexi01/TestXml01.java
@@ -0,0 +1,18 @@
+package com.ceshiren.xuexi01;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @Auther: ygg
+ * @Date: 2023/3/15 - 03 - 15 - 0:52
+ * @Description: com.ceshiren.xuexi01
+ * @version: 1.0
+ */
+public class TestXml01 {
+  @Test
+    void Testxml(){
+      given().when().then();
+  }
+}
